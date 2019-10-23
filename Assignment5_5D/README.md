455441cf9b2ffd29f581c22136efd2217d26e192
YifanZhu7 <48029358+YifanZhu7@users.noreply.github.com>
2019-10-22 22:25:25 -0400

    Add files via upload

---
+++ Assignment5_5D/README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# 5D_Fall19_Yifan_Zhu
+=======
+Describe the app
+Describe the app
+Assignment5_5D
+>>>>>>> first commit
---
+++ Assignment5_5D/src/main/java/com/example/Assignment5_5D/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assignment5_5D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
